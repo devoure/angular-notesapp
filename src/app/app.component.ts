8da6d6da247aba63d708b3aa7ca99faca77d0081
devoure <disguisedsandwich@gmail.com>
2024-06-10 17:39:06 +0300

    Added filters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,49 @@
 import { Component } from '@angular/core';
 import { CommonModule} from '@angular/common'
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router'
+import { FormsModule } from '@angular/forms'
 
 import { Tasks } from '../shared/models/tasks'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tasker';
+  newTask : string = ''
+  selectedOption : boolean[] = [true, false, false]
+  listFilter : string = '1'
 
-  tasks = [
-    // new Tasks('Learn Frontend'),
-    // new Tasks('Learn Ride a bike', true)
+  tasks : Tasks[] = [
+    new Tasks('Learn Frontend'),
+    new Tasks('Learn Ride a bike', true)
   ]
+
+  visibleTasks : Tasks[] = this.tasks
+
+  checkTask(task : Tasks){
+    task.isComplete = !task.isComplete
+    console.log(task)
+  }
+
+  addTask(){
+    console.log('>>> Adding ', this.newTask)
+    this.tasks.push(new Tasks(this.newTask))
+    this.newTask = ''
+  }
+
+  filterTasks(e : any){
+    console.log(">>> ", e.target.value)
+    if (e.target.value === '0'){
+      this.visibleTasks = this.tasks
+    } else if (e.target.value === '1'){
+      this.visibleTasks = this.tasks.filter(item => !item.isComplete)
+    } else{
+      this.visibleTasks = this.tasks.filter(item => item.isComplete)
+    }
+  }
 }
